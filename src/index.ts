bee2e6265dd6277c6d1a110399fa82aabe43b8bb
samoyed88 <12130383@me.mcu.edu.tw>
2024-04-10 19:11:50 +0800

    feat(ts): 利用陳列記錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import { Product } from "./product";
 
-const product = new Product(
-  1,
-  "圖像 Angular 開發入門 第二版",
-  "博碩文化",
-  500,
-  true,
-  new Date(2024, 3, 10)
-);
-console.log(product);
-console.table(product);
+const products = [
+  new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+  new Product(
+    2,
+    "金魚都能懂得 CSS 必學屬性",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+];
+console.log(products);
+console.table(products);
