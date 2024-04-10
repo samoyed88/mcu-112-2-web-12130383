47d64343d2d4e787a862b5ef7424967610f9d9e3
samoyed88 <12130383@me.mcu.edu.tw>
2024-04-10 18:48:23 +0800

    feat(ts): 利用類別定義產品資料

---
+++ src/product.ts
@@ -0,0 +1,26 @@
+export class Product {
+  constructor(
+    id: number,
+    name: String,
+    company: String,
+    price: number,
+    isShow: boolean,
+    createDate: Date,
+    modifyDate: undefined | Date
+  ) {
+    this.id = id;
+    this.name = name;
+    this.company = company;
+    this.price = price;
+    this.isShow = isShow;
+    this.createDate = createDate;
+    this.modifyDate = modifyDate;
+  }
+  id: number;
+  name: String;
+  company: String;
+  price: number;
+  isShow: boolean;
+  createDate: Date;
+  modifyDate: undefined | Date;
+}
