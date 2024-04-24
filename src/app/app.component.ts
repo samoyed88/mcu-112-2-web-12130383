0c2cd785227215dc2e512b488912370a82b26be1
samoyed88 <12130383@me.mcu.edu.tw>
2024-04-24 17:55:19 +0800

    feat(product): 產品卡片樣式實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ProductCardComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
