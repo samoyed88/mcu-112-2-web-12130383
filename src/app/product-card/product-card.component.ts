3ed303add389bdf14d2fd9d064d3a85c1493dc37
samoyed88 <12130383@me.mcu.edu.tw>
2024-04-24 19:30:48 +0800

    feat(product): 利用 DatePipe 顯示建檔日期

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,9 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output, booleanAttribute, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
@@ -12,10 +13,12 @@ export class ProductCardComponent {
   @Input() productName!: string;
   @Input() authors!: string;
   @Input() company!: string;
-  @Input({ transform: booleanAttribute }) isShow!: boolean;
   @Input() imgUrl!: string;
 
+  @Input({ transform: booleanAttribute }) isShow!: boolean;
   @Output() isShowChange = new EventEmitter<boolean>();
+
+  @Input() createDate!: Date;
 
   onsetDisplay(isShow: boolean): void {
     //this.isShow = isShow;
