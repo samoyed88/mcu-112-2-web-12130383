73e4a87c7aa3f61e7e4e197ee2ec19c5ae1ae650
samoyed88 <12130383@me.mcu.edu.tw>
2024-05-15 18:05:08 +0800

    feat(product): 實作資料新增與刪除後重新取得資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject, startWith, switchMap } from 'rxjs';
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { ProductService } from '../services/product.service';
@@ -16,8 +17,15 @@ export class ProductPageComponent implements OnInit {
 
   products!: Product[];
 
+  private readonly refresh$ = new Subject<void>();
+
   ngOnInit(): void {
-    this.productService.getList().subscribe((products) => (this.products = products));
+    this.refresh$
+      .pipe(
+        startWith(undefined),
+        switchMap(() => this.productService.getList())
+      )
+      .subscribe((products) => (this.products = products));
   }
   router = inject(Router);
 
@@ -31,14 +39,14 @@ export class ProductPageComponent implements OnInit {
       createDate: new Date(),
       price: 10000,
     });
-    this.productService.add(product).subscribe();
+    this.productService.add(product).subscribe(() => this.refresh$.next());
   }
 
   onEdit(product: Product): void {
     this.router.navigate(['product', 'form', product.id]);
   }
   onRemove({ id }: Product): void {
-    this.productService.remove(id).subscribe();
+    this.productService.remove(id).subscribe(() => this.refresh$.next());
   }
   onView(product: Product): void {
     this.router.navigate(['product', 'view', product.id]);
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, delay, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Product } from './../model/product';
 
 @Injectable({
@@ -55,7 +55,7 @@ export class ProductService {
   }
 
   getList(): Observable<Product[]> {
-    return of(this._data).pipe(delay(2000));
+    return of(this._data);
   }
 
   add(product: Product): Observable<Product> {
@@ -67,13 +67,13 @@ export class ProductService {
   update(product: Product): Observable<Product> {
     const index = this._data.findIndex(({ id }) => product.id === id);
     const newProduct = new Product({ ...product });
-    this._data[index];
+    this._data = [...this._data, newProduct];
     return of(newProduct);
   }
 
   remove(productId: number): Observable<Product> {
-    const index = this._data.findIndex(({ id }) => productId === id);
-    const product = this._data.splice(index, 1);
-    return of(product[0]);
+    const product = this._data.find(({ id }) => productId === id)!;
+    this._data = [...this._data.filter(({ id }) => productId !== id)];
+    return of(product);
   }
 }
